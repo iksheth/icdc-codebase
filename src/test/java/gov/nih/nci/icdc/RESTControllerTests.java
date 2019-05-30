58c8c0a634c0291e18a087c19e6f0911c4988bc7
jonkiky <jonkiky@gmail.com>
2019-05-30 16:19:32 -0400

    Add Junit test cases and why to generate REST Docs

--- src/test/java/gov/nih/nci/icdc/RESTControllerTests.java
+++ src/test/java/gov/nih/nci/icdc/RESTControllerTests.java
@@ -1,26 +1,51 @@
 package gov.nih.nci.icdc;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RESTControllerTests {
-	
-//	@Autowired
-//    private MockMvc mockMvc;
-	
+
+	private MockMvc mockMvc;
+
+	@Rule
+	public JUnitRestDocumentation jUnitRestDocumentation = new JUnitRestDocumentation();
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context)
+				.apply(documentationConfiguration(this.jUnitRestDocumentation)).build();
+	}
+
+	@Autowired
+	private WebApplicationContext context;
+
 	@Test
-    public void testPing() throws Exception {
-// 
-//		this.mockMvc.perform(get("/v1/rest/ping"))
-//        		.andDo(print()).andExpect(status().isOk())
-//                .andExpect(content().string(containsString("pong")));
-        
-    }
+	public void testPing() throws Exception {
+
+		this.mockMvc.perform(RestDocumentationRequestBuilders.get("/v1/rest/ping"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("pong")))
+				.andDo(document("{ClassName}/{methodName}"));
+
+	}
 
 }
