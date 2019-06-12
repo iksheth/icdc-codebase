13875d0e8a3131aa5d0483a49de2189c074322e4
jonkiky <jonkiky@gmail.com>
2019-06-12 14:42:29 -0400

    Refine endpoint structure

--- src/main/java/gov/nih/nci/icdc/controller/RESTController.java
+++ src/main/java/gov/nih/nci/icdc/controller/RESTController.java
@@ -12,52 +12,50 @@ import org.springframework.web.bind.annotation.RestController;
 import gov.nih.nci.icdc.service.Neo4JGraphQLService;
 
 @RestController
-@RequestMapping(value = "/v1/rest/")
 public class RESTController {
 
 	private static final Logger logger = LogManager.getLogger(RESTController.class);
 	@Autowired
 	private Neo4JGraphQLService neo4jService;
 
-
-	@RequestMapping(value = "/programs", method = RequestMethod.GET)
+	@RequestMapping(value = "/v1/ping", method = RequestMethod.GET)
+	@ResponseBody
+	public String ping() {
+		logger.info("hit end point:/ping");
+		return "pong";
+	}
+	
+	@RequestMapping(value = "/v1/rest/programs", method = RequestMethod.GET)
 	@ResponseBody
 	public String getPrograms() {
 		logger.info("hit end point:/programs");
-		String graphQL = "query {program {\n" + 
-				"   id\n" + 
-				"   state\n" + 
-				"   created_datetime\n" + 
-				"   updated_datetime\n" + 
-				"   name\n" + 
-				"   dbgap_accession_number\n" + 
-				"   submitter_id\n" + 
-				" }}";
+		String graphQL = "query {program {\n" + "   id\n" + "   state\n" + "   created_datetime\n"
+				+ "   updated_datetime\n" + "   name\n" + "   dbgap_accession_number\n" + "   submitter_id\n" + " }}";
 		return neo4jService.query(graphQL);
 	}
 
-	@RequestMapping(value = "/program/{id}/studies", method = RequestMethod.GET)
+	@RequestMapping(value = "/v1/rest/program/{id}/studies", method = RequestMethod.GET)
 	@ResponseBody
 	public String getProgramStudies(@PathVariable String id) {
 		String graphQL = "";
 		return neo4jService.query(graphQL);
 	}
 
-	@RequestMapping(value = "/studies", method = RequestMethod.GET)
+	@RequestMapping(value = "/v1/rest/studies", method = RequestMethod.GET)
 	@ResponseBody
 	public String getStudies() {
 		String graphQL = "";
 		return neo4jService.query(graphQL);
 	}
 
-	@RequestMapping(value = "/study/{id}/cases", method = RequestMethod.GET)
+	@RequestMapping(value = "/v1/rest/study/{id}/cases", method = RequestMethod.GET)
 	@ResponseBody
 	public String getStudyCases(@PathVariable String id) {
 		String graphQL = "";
 		return neo4jService.query(graphQL);
 	}
 
-	@RequestMapping(value = "/cases", method = RequestMethod.GET)
+	@RequestMapping(value = "/v1/rest/cases", method = RequestMethod.GET)
 	@ResponseBody
 	public String getCases() {
 		String graphQL = "";
