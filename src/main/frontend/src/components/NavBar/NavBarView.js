77ea6f1030cef45b0da0e6a24247b6a772f798ac
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2019-06-18 11:47:53 -0400

    Initial commit for dashboard and added some routes

---
+++ src/main/frontend/src/components/NavBar/NavBarView.js
@@ -0,0 +1,194 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  withStyles
+} from "@material-ui/core";
+import {
+  Menu as MenuIcon,
+  NotificationsNone as NotificationsIcon,
+  ArrowBack as ArrowBackIcon,
+} from "@material-ui/icons";
+import classNames from "classnames";
+import ProfileMenu from '../ProfileMenu/ProfileMenuView';
+
+
+import { Typography } from "../Wrappers/Wrappers";
+
+const NavBar = ({ classes, isSidebarOpened, toggleSidebar, ...props }) => (
+  <AppBar position="relative" className={classes.appBar} color="primary">
+    <Toolbar className={classes.toolbar}>
+      <IconButton
+        color="inherit"
+        onClick={toggleSidebar}
+        className={classNames(
+          classes.headerMenuButton,
+          classes.headerMenuButtonCollapse
+        )}
+      >
+        {isSidebarOpened ? (
+          <ArrowBackIcon
+            classes={{
+              root: classNames(classes.headerIcon, classes.headerIconCollapse)
+            }}
+          />
+        ) : (
+          <MenuIcon
+            classes={{
+              root: classNames(classes.headerIcon, classes.headerIconCollapse)
+            }}
+          />
+        )}
+      </IconButton>
+      <Typography variant="h6" weight="medium" className={classes.logotype}>Dashboard</Typography>
+      <Typography variant="h6" weight="medium" className={classes.logotype}>Programs</Typography>
+      <Typography variant="h6" weight="medium" className={classes.logotype}>Studies</Typography>
+      <Typography variant="h6" weight="medium" className={classes.logotype}>Cases</Typography>
+
+      <div className={classes.grow} />
+      <IconButton
+        color="inherit"
+        aria-haspopup="true"
+        aria-controls="mail-menu"
+        onClick={props.openNotificationsMenu}
+        className={classes.headerMenuButton}
+      >
+          <NotificationsIcon classes={{ root: classes.headerIcon }} />
+      </IconButton>
+      <IconButton
+        color="inherit"
+        aria-haspopup="true"
+        aria-controls="mail-menu"
+        onClick={props.openNotificationsMenu}
+        className={classes.headerMenuButton}
+      >
+          <NotificationsIcon classes={{ root: classes.headerIcon }} />
+      </IconButton>
+      <IconButton
+        color="inherit"
+        aria-haspopup="true"
+        aria-controls="mail-menu"
+        onClick={props.openNotificationsMenu}
+        className={classes.headerMenuButton}
+      >
+          <NotificationsIcon classes={{ root: classes.headerIcon }} />
+      </IconButton>
+      <ProfileMenu />
+    </Toolbar>
+  </AppBar>
+);
+
+const styles = theme => ({
+  logotype: {
+    color: "white",
+    marginLeft: theme.spacing.unit * 2.5,
+    marginRight: theme.spacing.unit * 2.5,
+    fontWeight: 500,
+    fontSize: 18,
+    whiteSpace: "nowrap",
+    [theme.breakpoints.down("xs")]: {
+      display: "none"
+    }
+  },
+  appBar: {
+    width: "100vw",
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  toolbar: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2
+  },
+  hide: {
+    display: "none"
+  },
+  grow: {
+    flexGrow: 1
+  },
+  messageContent: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  headerMenu: {
+    marginTop: theme.spacing.unit * 7
+  },
+  headerMenuList: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  headerMenuItem: {
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.primary.main,
+      color: "white"
+    }
+  },
+  headerMenuButton: {
+    marginLeft: theme.spacing.unit * 2,
+    padding: theme.spacing.unit / 2
+  },
+  headerMenuButtonCollapse: {
+    marginRight: theme.spacing.unit * 2
+  },
+  headerIcon: {
+    fontSize: 28,
+    color: "rgba(255, 255, 255, 0.35)"
+  },
+  headerIconCollapse: {
+    color: "white"
+  },
+  profileMenu: {
+    minWidth: 265
+  },
+  profileMenuUser: {
+    display: "flex",
+    flexDirection: "column",
+    padding: theme.spacing.unit * 2
+  },
+  profileMenuItem: {
+    color: theme.palette.text.hint
+  },
+  profileMenuIcon: {
+    marginRight: theme.spacing.unit * 2,
+    color: theme.palette.text.hint
+  },
+  profileMenuLink: {
+    fontSize: 16,
+    textDecoration: "none",
+    "&:hover": {
+      cursor: "pointer"
+    }
+  },
+  messageNotification: {
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+    "&:hover, &:focus": {
+      backgroundColor: theme.palette.background.light
+    }
+  },
+  messageNotificationSide: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginRight: theme.spacing.unit * 2
+  },
+  messageNotificationBodySide: {
+    alignItems: "flex-start",
+    marginRight: 0
+  },
+  sendMessageButton: {
+    margin: theme.spacing.unit * 4,
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    textTransform: "none"
+  },
+  sendButtonIcon: {
+    marginLeft: theme.spacing.unit * 2
+  }
+});
+
+export default withStyles(styles)(NavBar);
