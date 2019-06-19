52ffdeff1c030fe02058784d317cb13836f3bc49
Amit Mukherjee <akmukherjee@gmail.com>
2019-06-19 14:06:49 -0400

    Added 2 to Comment

--- src/test/java/gov/nih/nci/icdc/IcdcApplicationTests.java
+++ src/test/java/gov/nih/nci/icdc/IcdcApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IcdcApplicationTests {
-/* Adding Comment1  */
+/* Adding Comment2  */
 	@Test
 	public void contextLoads() {
 	}
