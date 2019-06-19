b7fd5bfa2af0c990191c0e34244b1447fa209606
Amit Mukherjee <akmukherjee@gmail.com>
2019-06-19 12:02:11 -0400

    Added Comment

--- src/test/java/gov/nih/nci/icdc/IcdcApplicationTests.java
+++ src/test/java/gov/nih/nci/icdc/IcdcApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IcdcApplicationTests {
-
+/* Application Tests */
 	@Test
 	public void contextLoads() {
 	}
