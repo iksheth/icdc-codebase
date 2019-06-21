fca310f7cb0df29114bd976ceff752a805fa6046
Yizhen Chen <jonkiky@gmail.com>
2019-06-21 10:29:22 -0400

    Remove unused import

--- src/main/java/gov/nih/nci/icdc/service/ValidationAspect.java
+++ src/main/java/gov/nih/nci/icdc/service/ValidationAspect.java
@@ -14,7 +14,6 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 
