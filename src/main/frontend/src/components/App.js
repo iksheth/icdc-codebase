ea5ab69e10cee7d86304663a49318d27f2b1dd24
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2019-06-25 14:10:28 -0400

    Adding the browser route

--- src/main/frontend/src/components/App.js
+++ src/main/frontend/src/components/App.js
@@ -9,7 +9,7 @@ const theme = createMuiTheme({...themes.default, ...overrides});
 
 const App = () => (
     <MuiThemeProvider theme={theme}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
       <Switch>
         <Route path="/" component={Layout} />
         {/* <Route component={Error} /> */}
