27bd03264593650a7e692041a89a1107f0bca5fc
Yizhen Chen <jonkiky@gmail.com>
2019-06-25 11:25:20 -0400

    Update the package.json add homepage as "/ICDC"
    npm run build not set relative paths to the assets

--- src/main/java/gov/nih/nci/icdc/IcdcApplication.java
+++ src/main/java/gov/nih/nci/icdc/IcdcApplication.java
@@ -1,14 +1,9 @@
 package gov.nih.nci.icdc;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import gov.nih.nci.icdc.model.SessionListener;
 
 @SpringBootApplication
 public class IcdcApplication extends SpringBootServletInitializer {
@@ -22,11 +17,5 @@ public class IcdcApplication extends SpringBootServletInitializer {
 		return application.sources(IcdcApplication.class);
 	}
 
-	
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		 super.onStartup(servletContext);
-		 servletContext.addListener(new SessionListener());
-	}
 
 }
