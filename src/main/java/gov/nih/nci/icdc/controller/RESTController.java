4b572de231ae72667d48e0ceaec912eafa48671e
Yizhen Chen <jonkiky@gmail.com>
2019-07-16 16:13:59 -0400

    update the REST API

--- src/main/java/gov/nih/nci/icdc/controller/RESTController.java
+++ src/main/java/gov/nih/nci/icdc/controller/RESTController.java
@@ -50,8 +50,12 @@ public class RESTController {
 	@ResponseBody
 	public String getPrograms(HttpServletRequest request, HttpServletResponse response) throws UnirestException {
 		logger.info("hit end point:/v1/rest/programs");
-		String graphQL = "query {program {\n" + "   id\n" + "   state\n" + "   created_datetime\n"
-				+ "   updated_datetime\n" + "   name\n" + "   dbgap_accession_number\n" + "   submitter_id\n" + " }}";
+		String graphQL = "query {"+"program {\n" + 
+				"  name\n" + 
+				"  program_description\n" + 
+				"  external_program_url\n" + 
+				"	}\n" + 
+				"}";
 		return neo4jService.query(graphQL);
 	}
 
@@ -71,9 +75,23 @@ public class RESTController {
 	@ResponseBody
 	public String getStudies(HttpServletRequest request, HttpServletResponse response) throws UnirestException {
 		logger.info("hit end point:/v1/rest/studies ");
-		String graphQL = "query {study{\n" + "  id\n" + "  state\n" + "  created_datetime\n" + "  updated_datetime\n"
-				+ "  project_id\n" + "  clinical_study_name\n" + "  clinical_study_designation\n"
-				+ "  clinical_study_id\n" + "  date_of_iacuc_approval\n" + "  submitter_id\n" +
+		String graphQL = "query {"+"study {\n" + 
+				"  clinical_study_id\n" + 
+				"  clinical_study_designation\n" + 
+				"  clinical_study_name\n" + 
+				"  clinical_study_description\n" + 
+				"  clinical_study_type\n" + 
+				"  date_of_iacuc_approval\n" + 
+				"  dates_of_conduct\n" + 
+				"  study_arms{\n" + 
+				"    arm\n" + 
+				"    ctep_treatment_assignment_code\n" + 
+				"  }\n" + 
+				"  study_sites{\n" + 
+				"   site_short_name\n" + 
+				"  veterinary_medical_center\n" + 
+				"  registering_institution\n" + 
+				"  }" + 
 				"}}";
 		return neo4jService.query(graphQL);
 	}
@@ -94,9 +112,89 @@ public class RESTController {
 	@ResponseBody
 	public String getCases(HttpServletRequest request, HttpServletResponse response) throws UnirestException {
 		logger.info("hit end point:/v1/rest/cases ");
-		String graphQL = "query {case {\n" + "id\n" + "  state\n" + "  created_datetime\n" + "  updated_datetime\n"
-				+ "  project_id\n" + "  patient_id\n" + "  patient_first_name\n" + "  crf_id\n" + "  submitter_id\n" +
-				" }}";
+		String graphQL = "query "+"{{case{\n" + 
+				"    patient_id\n" + 
+				"    patient_first_name\n" + 
+				"    crf_id\n" + 
+				"    cohort{\n" + 
+				"       cohort_description\n" + 
+				"       cohort_dose\n" + 
+				"       study_arm{\n" + 
+				"         arm\n" + 
+				"         ctep_treatment_assignment_code\n" + 
+				"         study{\n" + 
+				"              clinical_study_id\n" + 
+				"              clinical_study_designation\n" + 
+				"              clinical_study_name\n" + 
+				"              clinical_study_description\n" + 
+				"              clinical_study_type\n" + 
+				"              date_of_iacuc_approval\n" + 
+				"              dates_of_conduct\n" + 
+				"         }\n" + 
+				"         agents{\n" + 
+				"             medication\n" + 
+				"             document_number\n" + 
+				"         }\n" + 
+				"      }\n" + 
+				"    }\n" + 
+				"    enrollment{\n" + 
+				"        date_of_registration\n" + 
+				"        registering_institution\n" + 
+				"        initials\n" + 
+				"        date_of_informed_consent\n" + 
+				"        site_short_name\n" + 
+				"        veterinary_medical_center\n" + 
+				"        enrollment_document_number\n" + 
+				"        cohort_description\n" + 
+				"        patient_subgroup\n" + 
+				"    }\n" + 
+				"    demographic{\n" + 
+				"        breed\n" + 
+				"        patient_age_at_enrollment\n" + 
+				"        date_of_birth\n" + 
+				"        sex\n" + 
+				"        weight\n" + 
+				"        neutered_indicator\n" + 
+				"        crf_id\n" + 
+				"    }\n" + 
+				"    diagnoses{\n" + 
+				"       disease_term\n" + 
+				"        primary_disease_site\n" + 
+				"        stage_of_disease\n" + 
+				"        date_of_diagnosis\n" + 
+				"        histology_cytopathology\n" + 
+				"        date_of_histology_confirmation\n" + 
+				"        histological_grade\n" + 
+				"        pathology_report\n" + 
+				"        treatment_data\n" + 
+				"        follow_up_data\n" + 
+				"        concurrent_disease\n" + 
+				"        concurrent_disease_type\n" + 
+				"        crf_id\n" + 
+				"    }\n" + 
+				"    off_study{\n" + 
+				"        document_number\n" + 
+				"        date_off_study\n" + 
+				"        reason_off_study\n" + 
+				"        date_of_disease_progression\n" + 
+				"        date_off_treatment\n" + 
+				"        best_resp_vet_tx_tp_secondary_response\n" + 
+				"        date_last_medication_administration\n" + 
+				"        best_resp_vet_tx_tp_best_response\n" + 
+				"        date_of_best_response\n" + 
+				"    }\n" + 
+				"    off_treatment{\n" + 
+				"        document_number\n" + 
+				"        date_off_treatment\n" + 
+				"        reason_off_treatment\n" + 
+				"        date_of_disease_progression\n" + 
+				"        best_resp_vet_tx_tp_secondary_response\n" + 
+				"        date_last_medication_administration\n" + 
+				"        best_resp_vet_tx_tp_best_response\n" + 
+				"        date_of_best_response\n" + 
+				"    }\n" + 
+				"  }\n" + 
+				"}}";
 		return neo4jService.query(graphQL);
 	}
 
