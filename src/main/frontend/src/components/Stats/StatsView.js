2e02a067a85045f9c094c5a76e45cbd8ce65ade5
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2019-08-06 10:17:11 -0400

    codacy fixes

--- src/main/frontend/src/components/Stats/StatsView.js
+++ src/main/frontend/src/components/Stats/StatsView.js
@@ -13,62 +13,62 @@ import { GET_STATS } from './StatsController';
 const Stats = ({ classes, theme, ...props }) => {
     return (
         <Query query={GET_STATS}>
-            {({data, loading, error}) =>{
+            {({ data, loading, error }) => {
                 return (loading ? < CircularProgress /> : (error ? <Typography variant="headline" color="warning" size="sm">{error && `An error has occurred in loading stats component: ${error}`}</Typography> :
-        <Grid container spacing={32}>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}>
-                    <Grid container>
-                        <Grid item xs={1} ></Grid>
-                            <Grid item xs={12} sm={4} lg={2}>
-                                <Typography variant="headline" color="secondary" size="xxl">
-                                {data.numberOfStudies?data.numberOfStudies:0}          
+                    <Grid container spacing={32}>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <Grid container>
+                                    <Grid item xs={1} ></Grid>
+                                    <Grid item xs={12} sm={4} lg={2}>
+                                        <Typography variant="headline" color="secondary" size="xxl">
+                                            {data.numberOfStudies ? data.numberOfStudies : 0}
+                                        </Typography>
+                                        <Typography variant="headline" color="primary">
+                                            Studies
                                 </Typography>
-                                <Typography variant="headline" color="primary">
-                                  Studies
+                                    </Grid>
+                                    <Grid item xs={12} sm={4} lg={2}>
+                                        <Typography variant="headline" color="secondary" size="xxl">
+                                            {data.numberOfCases ? data.numberOfCases : 0}
+                                        </Typography>
+                                        <Typography variant="headline" color="primary">
+                                            Cases
                                 </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={4} lg={2}>
-                                <Typography variant="headline" color="secondary" size="xxl">
-                                {data.numberOfCases?data.numberOfCases:0}          
+                                    </Grid>
+                                    <Grid item xs={12} sm={4} lg={2}>
+                                        <Typography variant="headline" color="secondary" size="xxl">
+                                            {data.numberOfSamples ? data.numberOfSamples : 0}
+                                        </Typography>
+                                        <Typography variant="headline" color="primary">
+                                            Samples
                                 </Typography>
-                                <Typography variant="headline" color="primary">
-                                  Cases
+                                    </Grid>
+                                    <Grid item xs={12} sm={4} lg={2}>
+                                        <Typography variant="headline" color="secondary" size="xxl">
+                                            {data.numberOfFiles ? data.numberOfFiles : 0}
+                                        </Typography>
+                                        <Typography variant="headline" color="primary">
+                                            Files
                                 </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={4} lg={2}>
-                                <Typography variant="headline" color="secondary" size="xxl">
-                                {data.numberOfSamples?data.numberOfSamples:0}          
+                                    </Grid>
+                                    <Grid item xs={12} sm={4} lg={2}>
+                                        <Typography variant="headline" color="secondary" size="xxl">
+                                            {data.numberOfBiospecimenAliquots ? data.numberOfBiospecimenAliquots : 0}
+                                        </Typography>
+                                        <Typography variant="headline" color="primary">
+                                            Biospecimen Aliquots
                                 </Typography>
-                                <Typography variant="headline" color="primary">
-                                  Samples
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={4} lg={2}>
-                                <Typography variant="headline" color="secondary" size="xxl">
-                                {data.numberOfFiles?data.numberOfFiles:0}          
-                                </Typography>
-                                <Typography variant="headline" color="primary">
-                                  Files
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={4} lg={2}>
-                                <Typography variant="headline" color="secondary" size="xxl">
-                                {data.numberOfBiospecimenAliquots?data.numberOfBiospecimenAliquots:0}          
-                                </Typography>
-                                <Typography variant="headline" color="primary">
-                                  Biospecimen Aliquots
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={1}></Grid>
+                                    </Grid>
+                                    <Grid item xs={1}></Grid>
+                                </Grid>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                </Paper>
-            </Grid>
-        </Grid>
                 )
-                )
+                );
             }
-        }
+            }
         </Query>
     );
 };
