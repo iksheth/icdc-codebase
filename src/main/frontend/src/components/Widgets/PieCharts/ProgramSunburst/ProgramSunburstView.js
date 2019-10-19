dba213516076055f4caeb8bbe1e1ff83cff4dcc2
Yizhen Chen <jonkiky@gmail.com>
2019-10-18 22:31:41 -0400

    Fix eslint error

--- src/main/frontend/src/components/Widgets/PieCharts/ProgramSunburst/ProgramSunburstView.js
+++ src/main/frontend/src/components/Widgets/PieCharts/ProgramSunburst/ProgramSunburstView.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
 import React, { PureComponent } from 'react';
 import { Sunburst, LabelSeries } from 'react-vis';
 
 
 const LABEL_STYLE = {
-    fontSize: '15px',
-    textAnchor: 'middle'
+  fontSize: '15px',
+  textAnchor: 'middle',
 };
 
 function getKeyPath(node) {
@@ -14,85 +13,83 @@ function getKeyPath(node) {
   }
 
   return [(node.data && node.data.title) || node.title].concat(
-    getKeyPath(node.parent)
+    getKeyPath(node.parent),
   );
 }
 
 
-function updateData(data, keyPath) {
+function updateData(d, keyPath) {
+  const data = d;
   if (data.children) {
-    data.children.map(child => updateData(child, keyPath));
+    data.children.map((child) => updateData(child, keyPath));
   }
 
   data.style = {
     ...data.style,
-    fillOpacity: keyPath && !keyPath[data.title] ? 0.2 : 1
+    fillOpacity: keyPath && !keyPath[data.title] ? 0.2 : 1,
   };
 
   return data;
 }
 
 export default class ProgramSunburst extends PureComponent {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        pathValue: false,
-        widgetData: this.props.data,
-        finalValue: 'program studies sunburst',
-      };
-    }
-
+  constructor(props) {
+    super(props);
+    const { data } = this.props;
+    this.state = {
+      widgetData: data,
+      finalValue: 'program studies sunburst',
+    };
+  }
 
 
-    render() {
-        
-        const { finalValue,pathValue,widgetData} = this.state;
-        const {
-            width,
-            height
-        } = this.props;
+  render() {
+    const { finalValue, widgetData } = this.state;
+    const {
+      width,
+      height,
+    } = this.props;
 
-       
-        return (
-            <Sunburst
-                    hideRootNode
-                    animation
-                    colorType="literal"
-                    data={widgetData}
-                    height={height}
-                    width={width}
-                    style={{
-                        stroke: '#ddd',
-                        strokeOpacity: 0.3,
-                        strokeWidth: '0.5'
-                      }}
-                    onValueMouseOver={node => {
-                         
-                         const path = getKeyPath(node).reverse();
-                         const pathAsMap = path.reduce((res, row) => {
-                             res[row] = true;
-                             return res;
-                         }, {});
-                         const data = updateData(widgetData, pathAsMap);
-                         this.setState({
-                             finalValue: node.title,
-                             pathValue: path.join(' > '),
-                             data: data,
-                         });
-                         }
-                     }
-                     onValueMouseOut = {() => {
-                        this.setState({
-                             pathValue: false,
-                             finalValue: false,
-                             data: updateData(widgetData, false)
-                         })
-                       } 
-                     }
-            >
-            {finalValue && (<LabelSeries data={[{x: 0, y: 0, label: finalValue, style: LABEL_STYLE }]} /> )}
-         </Sunburst>
-        );
-    }
+
+    return (
+      <Sunburst
+        hideRootNode
+        animation
+        colorType="literal"
+        data={widgetData}
+        height={height}
+        width={width}
+        style={{
+          stroke: '#ddd',
+          strokeOpacity: 0.3,
+          strokeWidth: '0.5',
+        }}
+        onValueMouseOver={(node) => {
+          const path = getKeyPath(node).reverse();
+          const pathAsMap = path.reduce((res, row) => {
+            res[row] = true;
+            return res;
+          }, {});
+          const data = updateData(widgetData, pathAsMap);
+          this.setState({
+            finalValue: node.title,
+            widgetData: data,
+          });
+        }}
+        onValueMouseOut={() => {
+          this.setState({
+            finalValue: false,
+            widgetData: updateData(widgetData, false),
+          });
+        }}
+      >
+        {finalValue && (
+        <LabelSeries data={[{
+          x: 0, y: 0, label: finalValue, style: LABEL_STYLE,
+        }]}
+        />
+        )}
+      </Sunburst>
+    );
+  }
 }
--- src/main/frontend/src/pages/dashboard/caseTable/caseController.js
+++ src/main/frontend/src/pages/dashboard/caseTable/caseController.js
@@ -1,22 +1,14 @@
-/* eslint-disable */
 import React from 'react';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import { useSelector } from 'react-redux';
 import CaseView from './caseView';
-import { Typography } from '../../../components/Wrappers/Wrappers';
-import { useSelector } from 'react-redux'
 
 
 const caseContainer = () => {
-
-// data from store
- const   tableData  =useSelector(function(state){
-        return state.dashboard
-        	&&state.dashboard.datatable
-        		&&state.dashboard.datatable.data
-        			?state.dashboard.datatable.data:{};
-    } );
+  // data from store
+  const tableData = useSelector((state) => (state.dashboard
+        && state.dashboard.datatable
+        && state.dashboard.datatable.data
+    ? state.dashboard.datatable.data : {}));
 
 
   return <CaseView data={tableData} />;
