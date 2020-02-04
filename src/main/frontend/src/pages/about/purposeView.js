c5acfc9b9123dbecd774bd3233f9f58a6c5e04a8
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-02-04 14:42:59 -0500

    Adding github based content authering and package updates

--- src/main/frontend/src/pages/about/purposeView.js
+++ src/main/frontend/src/pages/about/purposeView.js
@@ -1,90 +1,39 @@
-import React from 'react';
-import { withStyles, Link } from '@material-ui/core';
-import Stats from '../../components/Stats/AllStatsController';
-import Header from '../../components/About/HeaderView';
-import l9dg from '../../assets/about/Photo-About_Purpose.jpg';
-import Body from '../../components/About/BodyView';
-import externalIcon from '../../assets/about/About-ExternalLink.svg';
+import React, { useState, useEffect } from 'react';
+import yaml from 'js-yaml';
+import axios from 'axios';
+import YAMLData from '../../content/prod/aboutPagesContent.yaml';
+import stockImg from '../../assets/about/Photo-About_Purpose.jpg';
+import AboutBody from '../../components/About/AbouBodyView';
 
-const PurposeView = ({ classes }) => (
-  <>
-    <Stats />
-    <Header title="Purpose" />
-    <Body data={{
-      img: l9dg,
-      body: (
-        <div>
-          {' '}
-NCI's Division of Cancer Treatment and Diagnosis (DCTD) charged
-the Frederick National Laboratory for Cancer Research (FNLCR) to
-build the integrated Canine Data Commons (ICDC), a cloud-based repository
-of canine cancer data. ICDC was established to further research
-on human cancers by enabling comparative
-analysis with canine cancer. The data in the ICDC is sourced
-from multiple different programs and projects; all focused on
-the canine subjects.
+const ABOUT_CONTENT_URL = process.env.REACT_APP_ABOUT_CONTENT_URL;
 
 
-   The data is harmonized
-  into an integrated data model and then made available to the research community.
-   The ICDC is part of the Cancer Research Data Commons (CRDC), an initiative
-   from NCI’s Center for Biomedical Informatics and Information Technology (CBIIT).
-   Bioinformatic analysis of the ICDC data is accomplished using the CRDC’s
+const SupportView = () => {
+  const [data, setData] = useState([]);
 
-          <Link
-            title="Cloud Resources."
-            target="_blank"
-            rel="noreferrer"
-            href="https://datascience.cancer.gov/data-commons/cloud-resources"
-            color="inherit"
-            className={classes.link}
-          >
-            {' '}
-            {' '}
-Cloud
-   Resources
-          </Link>
-          <img
-            src={externalIcon}
-            alt="outbounnd web site icon"
-            className={classes.linkIcon}
-          />
-.
-          <br />
-          <br />
-Within the FNLCR, the Biomedical Informatics and Data Science (BIDS) Directorate is
-focused on software engineering and data handling.  The Applied and Developmental
-Research Directorate (ADRD) is managing the ICDC Steering Committee and providing
- one of the data sources for the ICDC. Finally, there is also an ICDC Steering
- Committee (composed of external members, FNLCR and NCI staff) that provides advice
-  to DCTD on the ICDC.
-        </div>),
-    }}
-    />
-  </>
-);
+  useEffect(async () => {
+    let resultData = [];
+    try {
+      const result = await axios.get(ABOUT_CONTENT_URL);
+      resultData = yaml.safeLoad(result.data);
+    } catch (error) {
+      const result = await axios(YAMLData);
+      resultData = yaml.safeLoad(result.data);
+    }
+    const supportObj = resultData.find(({ page }) => page === '/purpose');
+    setData(supportObj);
+  }, []);
 
-const styles = () => ({
-  linkIcon: {
-    width: '20px',
-    verticalAlign: 'sub',
-    margin: '0px 0px 0px 2px',
-  },
-  link: {
-    color: '#0296C9',
-    fontWeight: 'bolder',
-    '&:hover': {
-      color: '#0296C9',
-      fontWeight: 'bolder',
-      textDecoration: 'none',
-    },
-  },
-  title: {
-    color: '#0B3556',
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-  },
-});
-
-
-export default withStyles(styles, { withTheme: true })(PurposeView);
+  return (
+    <>
+      <AboutBody data={{
+        img: stockImg,
+        title: data.title,
+        content: data.content,
+        table: data.table,
+      }}
+      />
+    </>
+  );
+};
+export default SupportView;
