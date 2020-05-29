a4bc046d01c8df15164b0831f248b719f9d60e90
jonkiky <jonkiky@gmail.com>
2020-05-29 12:51:23 -0400

    error message for wrong url

--- src/main/frontend/src/pages/programDetail/programDetailController.js
+++ src/main/frontend/src/pages/programDetail/programDetailController.js
@@ -4,17 +4,28 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import ProgramDetailView from './components/programDetailView';
 import { Typography } from '../../components/Wrappers/Wrappers';
 import { GET_PROGRAM_DETAIL_DATA_QUERY } from '../../utils/graphqlQueries';
-
+import Error from '../error/Error';
 
 const ProgramDetailContainer = ({ match }) => (
   <Query query={GET_PROGRAM_DETAIL_DATA_QUERY} variables={{ programTitle: match.params.id }}>
-    {({ data, loading, error }) => (
-      loading ? <CircularProgress />
-        : (
-          error || !data || !data.program[0] ? <Typography variant="headline" color="warning" size="sm">{error && `An error has occurred in loading stats component: ${error}`}</Typography>
-            : <ProgramDetailView data={data} />
-        )
-    )}
+    {({ data, loading, error }) => {
+      // loading status
+      if (loading) {
+        return <CircularProgress />;
+      }
+
+      // error as a internet error
+      if (error) {
+        return <Typography variant="headline" color="warning" size="sm">{error && `An error has occurred in loading component: ${error}`}</Typography>;
+      }
+
+      // error as not thing return from api
+      if (!data || data.program.length === 0) {
+        return <Error />;
+      }
+
+      return <ProgramDetailView data={data} />;
+    }}
   </Query>
 );
 
