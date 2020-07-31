07028462ab40bb9ea6610049ab35f60cf0b528c7
Karan Sheth <karan.sheth60@gmail.com>
2020-07-31 15:01:05 -0400

    Updated Cop Images & more
    1) Replaced Cop primary image with new image
    2) renamed cop's both images & moved cop's secondary image to cop folder.
    3) added alt text only for cop's primary image only in config file.

--- src/main/frontend/src/components/programImageConfig.js
+++ src/main/frontend/src/components/programImageConfig.js
@@ -1,10 +1,11 @@
-import copPrimary from '../assets/programCards/Program_Image.png';
-import copSecondary from '../assets/programCards/ProgramDetail_Image.jpg';
+import copPrimary from '../assets/programCards/cop/copPrimary.png';
+import copSecondary from '../assets/programCards/cop/copSecondary.jpg';
 
 const config = {
   COP: {
     prgramName: 'COP',
     primaryImage: copPrimary,
+    primaryImageAlt: 'The Center for Cancer Research (CCR) is the largest division of the NCI intramural research program and comprises nearly 250 basic and clinical research groups located on two campuses outside of Washington, DC.',
     secondaryImage: copSecondary,
   },
 };
